aa20c9ce75a90ddabfa55a5a3cf51a1330b84909
gnladu07 <gnlaud07@gmail.com>
2026-08-26 10:52:27 +0900

    feat: authorize 설정

--- src/main/java/com/planwith/planwith_fo_grade/config/OpenApiConfig.java
+++ src/main/java/com/planwith/planwith_fo_grade/config/OpenApiConfig.java
@@ -6,12 +6,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
+
+	private static final String BEARER_AUTH_SCHEME = "bearerAuth";
 
 	@Value("${app.gateway.public-url:/}")
 	private String gatewayPublicUrl;
@@ -25,10 +30,19 @@ public class OpenApiConfig {
 								Call APIs through the API Gateway (:8000).
 								Do not put Docker hostname or :8083 in OpenAPI servers.
 								Swagger Try-it-out must use the browser origin (Gateway).
+								Use Authorize to send HTTP Bearer JWT.
 								""")
 						.version("v1"))
 				.servers(List.of(new Server()
 						.url(gatewayPublicUrl)
-						.description("API Gateway")));
+						.description("API Gateway")))
+				.components(new Components()
+						.addSecuritySchemes(BEARER_AUTH_SCHEME, new SecurityScheme()
+								.name(BEARER_AUTH_SCHEME)
+								.type(SecurityScheme.Type.HTTP)
+								.scheme("bearer")
+								.bearerFormat("JWT")
+								.in(SecurityScheme.In.HEADER)))
+				.addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH_SCHEME));
 	}
 }
